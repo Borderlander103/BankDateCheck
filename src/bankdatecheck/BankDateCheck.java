d5434cb9c9e7d21674161c0d14c3f98c8a65e51f
Brian Richardson <brichardson@catalyte.io>
2021-03-15 17:58:22 -0700

    Add UK and HK holidays

--- src/bankdatecheck/BankDateCheck.java
+++ src/bankdatecheck/BankDateCheck.java
@@ -9,9 +9,11 @@ import java.util.List;
 public class BankDateCheck {
 
   static List<String> weekends = Arrays.asList("saturday", "sunday");
-  static List<String> holidays_2021 = Arrays.asList("2021-01-01", "2021-01-18", "2021-02-15",
+  static List<String> us_holidays_2021 = Arrays.asList("2021-01-01", "2021-01-18", "2021-02-15",
       "2021-04-02", "2021-04-19", "2021-05-31", "2021-07-05", "2021-09-06", "2021-10-11",
       "2021-11-11", "2021-11-25", "2021-11-26", "2021-12-24");
+  static List<String> uk_holidays_2021 = Arrays.asList("2021-01-01", "2021-04-02", "2021-04-05", "2021-05-03", "2021-05-31", "2021-08-30", "2021-12-27", "2021-12-28");
+  static List<String> hk_holidays_2021 = Arrays.asList("2021-01-01", "2021-02-12", "2021-02-15", "2021-04-02", "2021-04-05", "2021-04-06", "2021-05-19", "2021-06-14", "2021-07-01", "2021-09-22", "2021-10-01", "2021-10-14", "2021-12-27");
   static LocalDate date;
   static LocalTime time;
   static LocalTime openingTime = LocalTime.parse("09:00:00");
@@ -41,7 +43,7 @@ public class BankDateCheck {
   }
 
   public static void checkDate() {
-    while(holidays_2021.contains(date.toString()) || weekends.contains(date.getDayOfWeek().toString().toLowerCase())){
+    while(us_holidays_2021.contains(date.toString()) || weekends.contains(date.getDayOfWeek().toString().toLowerCase())){
       date = date.plusDays(1L);
       updateTime();
     }
